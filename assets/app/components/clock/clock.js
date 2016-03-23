697bd32b029dc4a55266c91c9c65f413ed825dc4
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2016-03-22 23:30:33 -0700

    Start using Moment

--- assets/app/components/clock/clock.js
+++ assets/app/components/clock/clock.js
@@ -2,15 +2,22 @@ import moment from 'moment';
 import clockTemplate from './clock.html';
 
 angular.module('App.components.clock', [])
-.directive('clock', function () {
+.directive('clock', function ($interval) {
   return {
     template: clockTemplate,
     restrict: 'A',
     scope: {
     },
     link: function (scope, element) {
+      scope.now = {
+        date: moment(),
+        textEu: 'Europe Format',
+        textUs: 'USA Format'
+      };
 
-
+      $interval(() => {
+        scope.now.date = moment();
+      }, 1000);
     }
   }
 });
